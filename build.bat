30193d51bda21c704a8750c40991f523234291ce
Ethan Zhao <ethan912yydegree@gmail.com>
2026-07-04 21:35:49 +0800

    chore: 删除误提交的临时文件

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=C:\Users\byy90\AppData\Local\Microsoft\WinGet\Packages\OpenJS.NodeJS.LTS_Microsoft.Winget.Source_8wekyb3d8bbwe\node-v24.16.0-win-x64;%PATH%
-cd /d D:\personal-blog\client
-call npm run build
-exit /b %ERRORLEVEL%
--- inspect-avatar.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const s = fs.readFileSync('D:\\personal-blog\\client\\src\\views\\ArticleDetail.vue', 'utf8');
-const idx = s.indexOf('.about-avatar .avatar-glow-ring');
-if (idx === -1) {
-  console.log('not found');
-} else {
-  console.log(JSON.stringify(s.substring(idx, idx + 700)));
-}
